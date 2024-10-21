a73013da112848c4ddbd41491070047fd53a7163
Magomed <maga_valiev@inbox.ru>
2024-10-21 20:46:54 +0300

    added validator for ph

--- soil/serializers.py
+++ soil/serializers.py
@@ -5,3 +5,9 @@ class SoilDataSerializer(serializers.ModelSerializer):
         model = SoilData
         fields = '__all__'
 
+    def validate(self, data):
+        if data['sender1_ph'] > 18 or data['sender1_ph'] < 0:
+            raise serializers.ValidationError("pH value for sender1 must be between 0 and 18.")
+        if data['sender2_ph'] > 18 or data['sender2_ph'] < 0:
+            raise serializers.ValidationError("pH value for sender2 must be between 0 and 18.")
+        return data
