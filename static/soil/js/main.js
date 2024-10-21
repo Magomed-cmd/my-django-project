ecd00af972d08f0b14f0ee8338f55153ab3e3622
Magomed <maga_valiev@inbox.ru>
2024-10-21 21:13:31 +0300

    star

--- static/soil/js/main.js
+++ static/soil/js/main.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
-    console.log("main.js loaded");  // Отладочное сообщение
+    console.log("main.js loaded");  // Отладочное сообщение, когда скрипт загружен
 
     const loader = document.getElementById("loader");
     const table = document.getElementById("data-table");
@@ -7,36 +7,54 @@ document.addEventListener("DOMContentLoaded", function() {
     table.style.display = "none";
 
     function fetchData() {
-        console.log("Fetching data...");  // Отладочное сообщение
+        console.log("Fetching data...");  // Отладочное сообщение перед началом запроса
 
         $.ajax({
             url: "/api/soil_data/",
             type: "GET",
             success: function(data) {
-                console.log("Данные получены:", data);  // Отладочное сообщение
+                console.log("Данные успешно получены:", data);  // Лог получения данных
 
                 var tableBody = $("#data-table tbody");
                 tableBody.empty();
 
                 for (var i = 0; i < data.length; i++) {
-                    console.log("Отображение строки данных:", data[i]);  // Отладка каждой строки данных
+                    console.log("Обработка строки данных:", data[i]);  // Лог для каждой строки данных
 
+                    // Проверка значений влажности и pH
+                    var sender1Moisture = data[i].sender1_moisture !== undefined ? data[i].sender1_moisture : "N/A";
+                    var sender2Moisture = data[i].sender2_moisture !== undefined ? data[i].sender2_moisture : "N/A";
+                    var sender1Ph = data[i].sender1_ph !== undefined ? data[i].sender1_ph : "N/A";  // Если pH отсутствует
+                    var sender2Ph = data[i].sender2_ph !== undefined ? data[i].sender2_ph : "N/A";  // Если pH отсутствует
+
+                    // Логи для каждого значения
+                    console.log("Sender 1 Moisture:", sender1Moisture);
+                    console.log("Sender 2 Moisture:", sender2Moisture);
+                    console.log("Sender 1 pH:", sender1Ph);
+                    console.log("Sender 2 pH:", sender2Ph);
+
+                    // Создание строки таблицы
                     var row = "<tr class='fade-in'>" +
                         "<td title='Timestamp: " + data[i].timestamp + "'>" + data[i].timestamp + "</td>" +
-                        "<td title='Sender 1 Moisture: " + data[i].sender1_moisture + "'>" + data[i].sender1_moisture + "</td>" +
-                        "<td title='Sender 2 Moisture: " + data[i].sender2_moisture + "'>" + data[i].sender2_moisture + "</td>" +
-                        "<td title='Sender 1 pH: " + data[i].sender1_ph + "'>" + data[i].sender1_ph + "</td>" +  // Добавляем данные pH
-                        "<td title='Sender 2 pH: " + data[i].sender2_ph + "'>" + data[i].sender2_ph + "</td>" +  // Добавляем данные pH
+                        "<td title='Sender 1 Moisture: " + sender1Moisture + "'>" + sender1Moisture + "</td>" +
+                        "<td title='Sender 2 Moisture: " + sender2Moisture + "'>" + sender2Moisture + "</td>" +
+                        "<td title='Sender 1 pH: " + sender1Ph + "'>" + sender1Ph + "</td>" +
+                        "<td title='Sender 2 pH: " + sender2Ph + "'>" + sender2Ph + "</td>" +
                         "</tr>";
-                    tableBody.append(row);
+
+                    tableBody.append(row);  // Добавление строки в таблицу
+                    console.log("Строка данных добавлена в таблицу.");  // Лог успешного добавления строки
                 }
-                loader.style.display = "none";
-                table.style.display = "table";
+
+                loader.style.display = "none";  // Скрыть лоадер
+                table.style.display = "table";  // Показать таблицу
                 gsap.fromTo(table, { opacity: 0 }, { opacity: 1, duration: 1 });
+                console.log("Таблица отображена.");  // Лог завершения отображения
             },
-            error: function() {
-                loader.style.display = "none";
+            error: function(jqXHR, textStatus, errorThrown) {
+                loader.style.display = "none";  // Скрыть лоадер в случае ошибки
                 alert("Failed to fetch data. Please try again later.");
+                console.error("Ошибка запроса:", textStatus, errorThrown);  // Лог ошибки запроса
             }
         });
     }
@@ -51,4 +69,5 @@ document.addEventListener("DOMContentLoaded", function() {
     $('html, body').animate({
         scrollTop: $(".table-container").offset().top
     }, 1000);
+    console.log("Анимация прокрутки к таблице завершена.");  // Лог завершения анимации
 });
